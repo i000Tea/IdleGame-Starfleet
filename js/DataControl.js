20d796a715bfdaed593b9aadfa2a93a3f0f0b7fb
i000Tea <107682438+i000Tea@users.noreply.github.com>
2024-06-01 11:24:51 +0800

    Update DataControl.js

--- js/DataControl.js
+++ js/DataControl.js
@@ -1,33 +1,45 @@
-const path = "https://raw.githubusercontent.com/i000Tea/IdleGame-Starfleet/main/data/"
+const path = "https://raw.githubusercontent.com/i000Tea/IdleGame-Starfleet/main/data/";
 
 // 定义一个变量用于存储 JSON 数据
 let jsonData_Btn = [];
 let jsonData_Res = [];
-$(document).ready(function () {
-    jsonData_Btn = LoadData("resData");
-    jsonData_Res = LoadData("btnData");
 
-    LoadOver();
+$(document).ready(function () {
+    // 使用Promise.all来等待所有的LoadData函数执行完毕
+    Promise.all([
+        LoadData("resData"),
+        LoadData("btnData")
+    ]).then(function(results) {
+        // 当所有的LoadData函数都成功执行后，将结果存储到jsonData_Btn和jsonData_Res中
+        jsonData_Btn = results[0];
+        jsonData_Res = results[1];
+        
+        // 执行LoadOver函数
+        LoadOver();
+    }).catch(function(error) {
+        console.error("加载数据失败: ", error);
+    });
 });
 
 
 function LoadData(jsonName) {
-    // 定义一个变量用于存储 JSON 数据
-    let getJson = [];
-    // 如果本地存储中没有缓存数据，则发送 AJAX 请求获取数据
-    $.getJSON(`${path}${jsonName}.json`, function (data) {
-        console.log("拉取GitHub");
-        // 将获取到的数据存储到本地存储中
-        localStorage.setItem('cachedData', JSON.stringify(data));
-        getJson = data;
-        console.log(getJson);
-    }).fail(function (jqxhr, textStatus, error) {
-        let err = textStatus + ", " + error;
-        console.error("请求失败: " + err);
+    return new Promise(function(resolve, reject) {
+        // 如果本地存储中没有缓存数据，则发送 AJAX 请求获取数据
+        $.getJSON(`${path}${jsonName}.json`, function (data) {
+            console.log("拉取GitHub");
+            // 将获取到的数据存储到本地存储中
+            localStorage.setItem('cachedData', JSON.stringify(data));
+            console.log(data);
+            resolve(data); // 成功获取数据，调用resolve函数
+        }).fail(function (jqxhr, textStatus, error) {
+            let err = textStatus + ", " + error;
+            console.error("请求失败: " + err);
+            reject(error); // 获取数据失败，调用reject函数
+        });
     });
-    return getJson;
 }
 
 function LoadOver(){
+    console.log("LoadOver");
     NewBtn();
 }
